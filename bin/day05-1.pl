e679adbbd22db2406ba6a92237dcf58d4bbd1c15
Owen Davies <owen@odavi.es>
2021-12-05 06:33:27 +0000

    Add day 5

---
+++ bin/day05-1.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+use v5.16;
+
+use List::Util qw(min max);
+
+my $points = {};
+my $overlaps = 0;
+
+while (<>) {
+    chomp;
+    my ($startx, $starty, $endx, $endy) = /^(\d+),(\d+) -> (\d+),(\d+)$/;
+    $points->{$startx} = {} if !defined $points->{$startx};
+
+    if ($startx == $endx) {
+        for my $y (min(($starty, $endy)) .. max(($starty, $endy))) {
+            mark($startx, $y);
+        }
+    } elsif ($starty == $endy) {
+        for my $x (min(($startx, $endx)) .. max(($startx, $endx))) {
+            mark($x, $starty);
+        }
+    }
+}
+
+sub mark {
+    my ($x, $y) = @_;
+
+    $points->{$x}->{$y}++;
+    $overlaps++ if ($points->{$x}->{$y} == 2);
+}
+
+say $overlaps;
