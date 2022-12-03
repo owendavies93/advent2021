9ca8c0f2c0c13c533cd9050664d81b60ff2578f4
Owen Davies <owen@odavi.es>
2022-12-03 17:57:20 +0000

    Use generic grid implementation in day 9

--- bin/day09-2.pl
+++ bin/day09-2.pl
@@ -4,27 +4,31 @@ use warnings;
 
 use v5.16;
 
+use lib '../cheatsheet/lib';
+
+use Advent::Grid::Dense::Square;
+
 use List::Util qw(product);
 use List::MoreUtils qw(all);
 
 my $width;
-my $height = 0;
-
 my @grid = ();
 while (<>) {
     chomp;
     my @l = split //;
     $width = scalar @l if !defined $width;
     push @grid, @l;
-    $height++;
 }
+
+my $g = Advent::Grid::Dense::Square->new({
+    grid => \@grid,
+    width => $width,
+});
 
 my @lows = ();
 for (my $i = 0; $i < scalar @grid; $i++) {
-    my $val = $grid[$i];
-    my @nis = neighbour_from_index($i);
-    my @nvs = map { $grid[$_] } @nis;
-    if (all { $val < $_ } @nvs) {
+    my $val = $g->get_at_index($i);
+    if (all { $val < $_ } $g->neighbour_vals_from_index($i)) {
         push @lows, $i;
     }
 }
@@ -37,7 +41,7 @@ for my $l (@lows) {
 
     while (scalar @q) {
         my $i = shift @q;
-        my @nis = neighbour_from_index($i);
+        my @nis = $g->neighbours_from_index($i);
         foreach my $ni (@nis) {
             if (!defined $seen->{$ni} && $grid[$ni] != 9) {
                 $seen->{$ni} = 1;
@@ -52,21 +56,3 @@ for my $l (@lows) {
 my @top3 = (sort { $b <=> $a } @basins)[0..2];
 say product @top3;
 
-sub check_bounds {
-    my $index = shift;
-    return ($index >= 0 && $index < scalar @grid);
-}
-
-sub neighbour_from_index {
-    my $i = shift;
-
-    my @cans;
-    if ($i % $width == 0) {
-        @cans = ($i + 1, $i + $width, $i - $width);
-    } elsif ($i % $width == ($width - 1)) {
-        @cans = ($i - 1, $i + $width, $i - $width);
-    } else {
-        @cans = ($i + 1, $i - 1, $i + $width, $i - $width);
-    }
-    return grep { check_bounds($_) } @cans;
-}
